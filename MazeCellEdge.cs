16ef9004aee54957480a5667dc671e0997f7867d
St-Jester <31212058+St-Jester@users.noreply.github.com>
2017-09-23 12:14:03 +0300

    Add files via upload

---
+++ MazeCellEdge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeAlgExam
+{
+    public abstract class MazeCellEdge
+    {
+        public bool isPlaced = false;
+        public bool isPickUp = false;
+        public bool isExit = false;
+        public MazeCell cell, otherCell;
+
+        public MazeDirection direction;
+
+        public virtual void Initialize(MazeCell cell, MazeCell otherCell, MazeDirection direction)
+        {
+            this.cell = cell;
+            this.otherCell = otherCell;
+            this.direction = direction;
+            cell.SetEdge(direction, this);
+        }
+    }
+}
